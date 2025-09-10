9c8a7df1134e5ab6633d2d2b89725425fecdd270
ChinmayLale <chinmay29.lale@gmail.com>
2025-09-10 19:07:29 +0530

    Changed origin

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1",
+   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://slate-x-server.vercel.app",
    withCredentials: true, // allows cookies/tokens if needed
 });
 
