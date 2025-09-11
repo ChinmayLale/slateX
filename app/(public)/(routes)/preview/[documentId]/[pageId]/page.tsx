a5af2db7d66bddcf8655462012cb2958cdbaf6ff
ChinmayLale <chinmay29.lale@gmail.com>
2025-09-11 15:26:34 +0530

    Big Fix2

--- app/(public)/(routes)/preview/[documentId]/[pageId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/[pageId]/page.tsx
@@ -50,21 +50,6 @@ function DocumentIdPage() {
     );
   };
 
-  if (page === null) {
-    return (
-      <div>
-        <CoverImage.Skeleton />
-        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
-          <div className="space-y-4 pl-8 pt-4 ">
-            <Skeleton className="h-4 w-[40%]" />
-            <Skeleton className="h-14 w-[80%]" />
-            <Skeleton className="h-14 w-[70%]" />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   const getInitialContent = async (
     initialContent: string
   ): Promise<PartialBlock[] | undefined> => {
@@ -109,6 +94,21 @@ function DocumentIdPage() {
 
     fetchContent();
   }, [page]);
+
+  if (page === null) {
+    return (
+      <div>
+        <CoverImage.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4 ">
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-14 w-[80%]" />
+            <Skeleton className="h-14 w-[70%]" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   if (pageData === undefined) {
     return (
